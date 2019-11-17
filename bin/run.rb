8bd06716288d3f0bb85c27ad177ea295f8535a0b
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-17 03:40:31 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,8 @@ Sinatra
 Octokit
 Pry
 AwesomePrint
+
+# gem "sinatra", "1.4.4"
+# gem "hashie"
+# gem "octokit", "~> 2.0"
+# gem "awesome_print", git: "git@github.com:awesome-print/awesome_print.git"
